c739e7db87a5216c5b50aab96642adbd7d7e51bc
MaksimusD <Maksim@MacBook-Pro-admin.local>
2023-10-27 16:21:07 +0300

    add int type

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,7 +2,8 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Push world!");
+        System.out.println("Hello My world!");
     }
-int push = 1;
+int raz = 1;
+    int dva = 2;
 }
