595c900901a9f6a6fd8aff2fb9daa287257f69a2
MaksimusD <Maksim@MacBook-Pro-admin.local>
2023-10-27 16:31:57 +0300

    update

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello My world!");
+        System.out.println("Hello My3 world!");
     }
 int raz = 1;
     int dva = 2;
